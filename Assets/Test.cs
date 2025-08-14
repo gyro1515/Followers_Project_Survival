6f881c9813c98a98784fcec61d3c6e882c9558ff
gyro1515 <gyro1515@naver.com>
2025-08-14 11:43:24 +0900

    [Init] utf-8로 자동 저장되게 하기

---
+++ Assets/Test.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // 한글 주석 테스트용 스크립트 입니다.
+    // UTF-8로 설정 시 한글이 깨지지 않습니다.
+}
