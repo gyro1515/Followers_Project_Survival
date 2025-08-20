3ce10a80810b74806c7dabdb8ad114ef7c9c4ead
gyro1515 <gyro1515@naver.com>
2025-08-20 12:08:49 +0900

    [Feat] NPC도 리스폰 컨트롤러에서 자원과 안 겹치게 소환하기 구현

--- Assets/Scripts/Resource/ResourceRespawnController.cs
+++ Assets/Scripts/Resource/ResourceRespawnController.cs
@@ -15,7 +15,8 @@ public class ResourceRespawnController : MonoBehaviour
     [SerializeField] private float spawnRadius = 40;   // 리스폰 영역 반경(맵 크기)
     [SerializeField] private float minDistance = 3f;    // 기존 자원과 최소 거리
     [SerializeField] private float respownTime = 0.5f; // 리스폰 타임
-    public List<ItemData> testInventory = new List<ItemData>(); // 테스트용
+    [Header("NPC 목록")]
+    [SerializeField] private List<GameObject> npcPrefabs = new List<GameObject>();
 
     private int curTreeCnt = 0;
     private int curRockCnt = 0;
@@ -26,8 +27,9 @@ public class ResourceRespawnController : MonoBehaviour
     // 상호작용 테스트용, 플레이어로 옮겨야 함
     IInteractable curInteractable;
 
-    private void Start()
+    private void Awake()
     {
+        SpawnNPC(); // npc와 자원이 안 겹치도록 소환하기
         InvokeRepeating("Respawn", 0f, respownTime); // respownTime초마다 리스폰
     }
     private void Update()
@@ -73,6 +75,48 @@ public class ResourceRespawnController : MonoBehaviour
         curInteractable = interactableForText;
         interactableForText.SetInteractionText();
 
+    }
+    void SpawnNPC()
+    {
+        foreach(var npc in npcPrefabs)
+        {
+            CheckSpawnNPC(npc);
+        }
+    }
+    void CheckSpawnNPC(GameObject prefab)
+    {
+        // NPC는 무조건 소환하기
+        while (true)
+        {
+            // 랜덤 좌표
+            Vector3 randomPos = gameObject.transform.position + Random.insideUnitSphere.normalized * Random.Range(0f, spawnRadius);
+            // x, z값만 저장해서 쓰기
+            Vector2 randomPos2;
+            randomPos2.x = randomPos.x;
+            randomPos2.y = randomPos.z;
+            // 중복된 좌표인가, 중복되었다면 다시 좌표 생성
+            if (spawnedPos.Contains(randomPos2)) continue;
+            // 기존 물체와의 거리 체크
+            bool bCanSpawn = true;
+            foreach (Vector2 pos in spawnedPos)
+            {
+                // 기존 물체와의 거리 체크하기
+                if (Vector2.Distance(randomPos2, pos) > minDistance) continue;
+                bCanSpawn = false;
+                break;
+            }
+            // 소환 불가라면 다음 좌표 생성
+            if (!bCanSpawn) continue;
+            // 기존 물체와 거리상의 문제가 없다면 땅 위 좌표 찾기
+            // 땅이 없다면 다음 좌표 생성
+            if (!Physics.Raycast(randomPos + Vector3.up * 50f, Vector3.down, out RaycastHit hit, 100f, groundLayer)) continue;
+            Vector3 spawnPos = hit.point;
+            // 랜덤 회전값 주기, y값만(yaw)
+            float randomY = Random.Range(0f, 360f);
+            Quaternion rot = Quaternion.Euler(0f, randomY, 0f);
+            Instantiate(prefab, spawnPos, rot).transform.SetParent(gameObject.transform);
+            break; // 소환 끝나면 while 종료
+        }
     }
     void Respawn()
     {
